bb1c850875d3d59f33da3cab56f82a9e8af0688b
jmc27 <jmc27@brandeis.edu>
2014-06-12 12:33:38 -0400

    Parse Test object success

--- coloradoconcerts/platforms/android/ant-gen/R.java.d
+++ coloradoconcerts/platforms/android/ant-gen/R.java.d
@@ -14,4 +14,17 @@
 /Users/jchu/Desktop/ColoradoConcerts/Colorado-Concerts/coloradoconcerts/platforms/android/res/drawable-xhdpi/icon.png \
 /Users/jchu/Desktop/ColoradoConcerts/Colorado-Concerts/coloradoconcerts/platforms/android/res/values/strings.xml \
 /Users/jchu/Desktop/ColoradoConcerts/Colorado-Concerts/coloradoconcerts/platforms/android/res/xml/config.xml \
+/Users/jchu/Desktop/ColoradoConcerts/Colorado-Concerts/coloradoconcerts/platforms/android/ant-build/res/drawable/icon.png \
+/Users/jchu/Desktop/ColoradoConcerts/Colorado-Concerts/coloradoconcerts/platforms/android/ant-build/res/drawable-hdpi/icon.png \
+/Users/jchu/Desktop/ColoradoConcerts/Colorado-Concerts/coloradoconcerts/platforms/android/ant-build/res/drawable-land-hdpi/screen.png \
+/Users/jchu/Desktop/ColoradoConcerts/Colorado-Concerts/coloradoconcerts/platforms/android/ant-build/res/drawable-land-ldpi/screen.png \
+/Users/jchu/Desktop/ColoradoConcerts/Colorado-Concerts/coloradoconcerts/platforms/android/ant-build/res/drawable-land-mdpi/screen.png \
+/Users/jchu/Desktop/ColoradoConcerts/Colorado-Concerts/coloradoconcerts/platforms/android/ant-build/res/drawable-land-xhdpi/screen.png \
+/Users/jchu/Desktop/ColoradoConcerts/Colorado-Concerts/coloradoconcerts/platforms/android/ant-build/res/drawable-ldpi/icon.png \
+/Users/jchu/Desktop/ColoradoConcerts/Colorado-Concerts/coloradoconcerts/platforms/android/ant-build/res/drawable-mdpi/icon.png \
+/Users/jchu/Desktop/ColoradoConcerts/Colorado-Concerts/coloradoconcerts/platforms/android/ant-build/res/drawable-port-hdpi/screen.png \
+/Users/jchu/Desktop/ColoradoConcerts/Colorado-Concerts/coloradoconcerts/platforms/android/ant-build/res/drawable-port-ldpi/screen.png \
+/Users/jchu/Desktop/ColoradoConcerts/Colorado-Concerts/coloradoconcerts/platforms/android/ant-build/res/drawable-port-mdpi/screen.png \
+/Users/jchu/Desktop/ColoradoConcerts/Colorado-Concerts/coloradoconcerts/platforms/android/ant-build/res/drawable-port-xhdpi/screen.png \
+/Users/jchu/Desktop/ColoradoConcerts/Colorado-Concerts/coloradoconcerts/platforms/android/ant-build/res/drawable-xhdpi/icon.png \
 /Users/jchu/Desktop/ColoradoConcerts/Colorado-Concerts/coloradoconcerts/platforms/android/ant-build/AndroidManifest.xml \
